7706bc432ffc04d428784d15f4d490ff8c61ce78
3ndrius <andrzejo1991@gmail.com>
2018-03-21 15:10:07 +0100

    Single item

--- src/app/services/firebase.service.ts
+++ src/app/services/firebase.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
-import { AngularFire, FirebaseListObservable} from 'angularfire2';
+import { AngularFire, FirebaseListObservable, FirebaseObjectObservable} from 'angularfire2';
 
 
 @Injectable()
 export class FirebaseService {
 
   listings: FirebaseListObservable<any[]>;
+  listing: FirebaseObjectObservable<any[]>;
   constructor(private af:AngularFire) { }
 
   getListings() {
@@ -15,6 +16,12 @@ export class FirebaseService {
 
   }
 
+  getListingDetails(id) {
+    this.listing = this.af.database.object('/listings/'+id) as FirebaseObjectObservable<Listing>;
+    return this.listing;
+    
+    
+  }
 }
 
 
