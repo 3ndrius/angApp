2b7967dcf9892c6481e17fe54da9fde68a9d41f3
3ndrius <3ndrius@users.noreply.github.com>
2018-08-25 17:45:20 +0200

    Update firebase.service.ts

--- src/app/services/firebase.service.ts
+++ src/app/services/firebase.service.ts
@@ -1,32 +1,22 @@
 import { Injectable } from '@angular/core';
 import { AngularFire, FirebaseListObservable, FirebaseObjectObservable} from 'angularfire2';
 import * as firebase from 'firebase';
-
 @Injectable()
 export class FirebaseService {
-
   listings: FirebaseListObservable<any[]>;
   listing: FirebaseObjectObservable<any>;
   folder:any;
-
   constructor(private af:AngularFire) { 
     this.folder= 'listingimages';
   }
-
   getListings() {
-
     this.listings = this.af.database.list('/listings') as FirebaseListObservable<Listing[]>;
     return this.listings;
-
   }
-
   getListingDetails(id) {
     this.listing = this.af.database.object('/listings/'+id) as FirebaseObjectObservable<Listing>;
     return this.listing;
-    
-    
   }
-
   addListing(listing) {
 
     let storageRef = firebase.storage().ref();
@@ -34,15 +24,13 @@ export class FirebaseService {
      
       let path = `/${this.folder}/${selectedFile.name}`;
       let iRef = storageRef.child(path);
-      iRef.put(selectedFile).then((snapshot) =>{
-        
+      iRef.put(selectedFile).then((snapshot) =>{ 
         listing.image = selectedFile.name;
         listing.path = path;
         return this.listings.push(listing);
       });
     }
   }
-
   updateListing(id, listing){
     return this.listings.update(id, listing);
   }
@@ -50,13 +38,7 @@ export class FirebaseService {
   deleteListing(id){
     return this.listings.remove(id);
   }
-
-
-
-
 }
-
-
 interface Listing{
   key?:string;
   title?:string;
